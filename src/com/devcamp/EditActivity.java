c7650b0fb8ece9418f7deef0f3015b7aab74bd00
Wojciech Langiewicz <wlangiewicz@gmail.com>
2012-02-18 14:17:20 +0100

    added edit fields  + setting text in edit action

--- src/com/devcamp/EditActivity.java
+++ src/com/devcamp/EditActivity.java
@@ -9,21 +9,41 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class EditActivity extends Activity implements OnClickListener {
+	EditText start_time;
+	EditText end_time;
+	EditText radius;
+	Spinner mode;
+	EditText priority;
+	EditText latitude;
+	EditText longtitude;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.edit_layout);
 		Intent i = getIntent();
 		
+		start_time = (EditText)findViewById(R.id.start_time);
+		
 		if(i.getAction().equals(Intent.ACTION_EDIT)){ // wypełnij pola
 			Uri u = i.getData();
 			Cursor c = managedQuery(u, null, null, null, null);
 			c.moveToFirst();
-			//c.getString(c.getColumnIndex(Profile.))
+			start_time.setText(c.getString(c.getColumnIndex(Profile.START)));
+			end_time.setText(c.getString(c.getColumnIndex(Profile.STOP)));
+			c.getString(c.getColumnIndex(Profile.LATITUDE));
+			c.getString(c.getColumnIndex(Profile.LONGITUDE));
+			radius.setText(c.getString(c.getColumnIndex(Profile.RADIUS)));
+			c.getString(c.getColumnIndex(Profile.MODE));
+			priority.setText(c.getString(c.getColumnIndex(Profile.PRIORITY)));
 		}
-		//puste pola
+		else{//puste pola
+			
+		}
 		
 	}
 
