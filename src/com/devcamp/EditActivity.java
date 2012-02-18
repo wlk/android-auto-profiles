928e2a219338af9b6a32d40f46aff853de780353
Wojciech Langiewicz <wlangiewicz@gmail.com>
2012-02-18 11:51:37 +0100

    ready for editlayout

---
+++ src/com/devcamp/EditActivity.java
@@ -0,0 +1,15 @@
+package com.devcamp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EditActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.edit_layout);
+		
+		
+	}
+
+}
