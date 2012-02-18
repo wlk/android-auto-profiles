9dbf90d08d0d88b2374c12246a88ad370bb14626
Wojciech Langiewicz <wlangiewicz@gmail.com>
2012-02-18 11:21:08 +0100

    edit layout (xml only)

--- src/com/devcamp/SettingsActivity.java
+++ src/com/devcamp/SettingsActivity.java
@@ -29,8 +29,9 @@ public class SettingsActivity extends ListActivity {
 		lv.setOnItemClickListener(new OnItemClickListener() {
 			public void onItemClick(AdapterView<?> parent, View view,
 					int position, long id) {
-				Toast.makeText(getApplicationContext(),
-						((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+				
+//				Toast.makeText(getApplicationContext(),
+//						((TextView) view).getText(), Toast.LENGTH_SHORT).show();
 			}
 
 		});
