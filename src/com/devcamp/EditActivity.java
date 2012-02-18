1904d7cb71d943eb2bc00eb120331cf46e4fd16a
Wojciech Langiewicz <wlangiewicz@gmail.com>
2012-02-18 15:08:11 +0100

    edit view ok

--- src/com/devcamp/EditActivity.java
+++ src/com/devcamp/EditActivity.java
@@ -5,8 +5,12 @@ import android.widget.Button;
 import com.devcamp.provider.ProfileContract.Profile;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.database.Cursor;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -20,22 +24,34 @@ public class EditActivity extends Activity implements OnClickListener {
 	EditText start_time;
 	EditText end_time;
 	EditText radius;
-	Spinner mode; //aby pobrać użyj getPosition lub coś podobnego
+	Spinner mode; // aby pobrać użyj getPosition lub coś podobnego
 	EditText priority;
 	EditText latitude;
 	EditText longtitude;
+	
+	
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		
+		
 		setContentView(R.layout.edit_layout);
+		
+
+		
 		Intent i = getIntent();
 
 		start_time = (EditText) findViewById(R.id.start_time);
+		end_time = (EditText) findViewById(R.id.end_time);
+		radius = (EditText) findViewById(R.id.radius);
+		priority = (EditText) findViewById(R.id.priority);
+		latitude = (EditText) findViewById(R.id.latitude);
+		longtitude = (EditText) findViewById(R.id.longtitude);
+		mode = (Spinner) findViewById(R.id.mode);
 
-		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
-				this, R.array.priorities,
-				android.R.layout.simple_spinner_item);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.priorities, android.R.layout.simple_spinner_item);
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		mode.setAdapter(adapter);
 
@@ -52,25 +68,26 @@ public class EditActivity extends Activity implements OnClickListener {
 			String modeString = c.getString(c.getColumnIndex(Profile.MODE));
 			Integer m = Integer.valueOf(modeString);
 			mode.setSelection(m);
-			
-			//priority.setText(c.getString(c.getColumnIndex(Profile.PRIORITY)));
+
+			// priority.setText(c.getString(c.getColumnIndex(Profile.PRIORITY)));
 		} else {// puste pola
 
 		}
 
-        final Button b = (Button) findViewById(R.id.save_profile_button);
-        b.setOnClickListener(this);
+		final Button b = (Button) findViewById(R.id.save_profile_button);
+		b.setOnClickListener(this);
 	}
 
 	public void onClick(View v) {
-        saveProfile();
-        finish();
+		saveProfile();
+		finish();
 	}
 
-    public void setProfileListeners() {
-    }
+	public void setProfileListeners() {
 
-    public void saveProfile() {
+	}
+
+	public void saveProfile() {
         Intent i = getIntent();
         Uri u = i.getData();
 
@@ -85,13 +102,35 @@ public class EditActivity extends Activity implements OnClickListener {
 
         ProfileProvider provider = new ProfileProvider();
 
-        if(i.getAction().equals(Intent.ACTION_EDIT)){
+        if(i.getAction().equals(Intent.ACTION_INSERT)){
             provider.insert(u, values);
-        } else {
-            //provider.update(u, values);
+        } 
+        if(i.getAction().equals(Intent.ACTION_EDIT)){
+        	//provider.update(u, values);
         }
+        
+        
 
         setProfileListeners();
     }
+
+//	private void registerLocationObserver(double longitude, double latitude, int r) {
+//		LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+//		locationManager.addProximityAlert(latitude, longitude, r, -1, proximityIntent);
+//		IntentFilter filter = new IntentFilter(PROX_ALERT_INTENT);
+//		registerReceiver(new ProximityIntentReceiver(), filter);
+//	}
+//
+//	private void registerTimeObserver(String start_time, String end_time) {
+//		Intent myIntent = new Intent(AndroidAlarmService.this, MyAlarmService.class);
+//		pendingIntent = PendingIntent.getService(AndroidAlarmService.this, 0, myIntent, 0);
+//
+//		AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+//
+//		Calendar calendar = Calendar.getInstance();
+//		calendar.setTimeInMillis(System.currentTimeMillis());
+//		calendar.add(Calendar.SECOND, 10);
+//		alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), 5 * 1000, pendingIntent);
+//	}
 
 }
