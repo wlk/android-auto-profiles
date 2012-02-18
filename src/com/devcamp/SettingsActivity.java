a7d9fbe069cf4d3249a02853fe739985c46ea06d
Wojciech Langiewicz <wlangiewicz@gmail.com>
2012-02-18 13:41:52 +0100

    edit intent

--- src/com/devcamp/SettingsActivity.java
+++ src/com/devcamp/SettingsActivity.java
@@ -1,41 +1,63 @@
 package com.devcamp;
 
+import com.devcamp.provider.ProfileContract.Profile;
+
 import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.*;
+import android.widget.AdapterView.OnItemClickListener;
 
-public class SettingsActivity extends ListActivity {
+public class SettingsActivity extends ListActivity implements OnClickListener {
 
-	static final String[] test = new String[] { "silent", "test", "costam",
-			"dupa" };
+
+	static final String[] from = new String[] { Profile.NAME };
+
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-        View footer = getLayoutInflater().inflate(R.layout.list_footer, null);
+		Cursor c = managedQuery(Profile.CONTENT_URI, null, null, null, null);
 
-        ListView lv = getListView();
-        lv.setTextFilterEnabled(true);
+		View footer = getLayoutInflater().inflate(R.layout.list_footer, null);
 
-        lv.addFooterView(footer);
+		ListView lv = getListView();
+		lv.setTextFilterEnabled(true);
 
-        setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, test));
+		lv.addFooterView(footer);
 
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+		final Button b = (Button) findViewById(R.id.add_profile_button);
+		b.setOnClickListener(this);
+
+		int[] to = new int[] { R.id.list_item_id };
+
+		setListAdapter(new SimpleCursorAdapter(this, R.layout.list_item, c,
+				from, to));
+
+		lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 			public void onItemClick(AdapterView<?> parent, View view,
 					int position, long id) {
-				Intent i = new Intent(getApplicationContext(), EditActivity.class);
-				i.putExtra("profile_index", 1);
-				
-//				Toast.makeText(getApplicationContext(),
-//						((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+				Intent i = new Intent();
+				i.setData(Profile.CONTENT_URI.buildUpon().appendPath(String.valueOf(id)).build());
+				i.setAction(Intent.ACTION_EDIT);
+				startActivity(i);
 			}
 
 		});
+	}
+
+	public void onClick(View v) {
+		Intent i = new Intent();
+		i.setData(Profile.CONTENT_URI);
+		i.setAction(Intent.ACTION_INSERT);
+		startActivity(i);
+		
+
 	}
 }
