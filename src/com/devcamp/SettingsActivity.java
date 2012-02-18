285299ca946736df197253f5efd42850d666f82a
Wojciech Langiewicz <wlangiewicz@gmail.com>
2012-02-18 10:47:08 +0100

    eclipse project

--- src/com/devcamp/SettingsActivity.java
+++ src/com/devcamp/SettingsActivity.java
@@ -1,13 +1,20 @@
 package com.devcamp;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
-public class SettingsActivity extends Activity {
-    /** Called when the activity is first created. */
+public class SettingsActivity extends ListActivity {
+    
+    
+    static final String[] test  = new String[] { "silent", "test", "costam", "dupa"};
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, test));
     }
 }
